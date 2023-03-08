8f904062a65d2e3dc47711fde939f8dd99f4a1de
Saketh Reddy <yssaketh@gmail.com>
2023-03-08 00:41:18 -0700

    Started working with IS

--- scripts/callingConvention.py
+++ scripts/callingConvention.py
@@ -3,4 +3,5 @@ import angr
 p = angr.Project('../executables/fauxware', auto_load_libs=False)
 
 # Determine the function calling convention of the binary
-print("Calling convention: ", p.arch.calling_convention)
+print(dir(p.factory.cc()))
+print(p.factory.cc)
